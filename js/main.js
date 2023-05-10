7b0aa346736394cbf6d0f3ce94b373b673be6933
UlyssesVertpage <90066493+UlyssesVertpage@users.noreply.github.com>
2023-05-10 15:09:13 +0300

    Task 4, fix for /js/main.js: A few problems occurred when checking last commit on GitHub were fixed.

--- js/main.js
+++ js/main.js
@@ -1,27 +1,46 @@
-'use strict';
-
-
 
 // #1
 // Gets random number from range including boundaries by floor:
 function getRandomNumberFromRange(a, b) {
-  if (a < 0 || b < 0) return -1;
+  if (a < 0 || b < 0) {
+    return -1;
+  }
   return Math.floor((b >= a)
-      ? Math.random() * (b + 1 - a) + a
-      : Math.random() * (a + 1 - b) + b);
+    ? Math.random() * (b + 1 - a) + a
+    : Math.random() * (a + 1 - b) + b);
 }
-// A Code Convention wasn't read yet so two tabs at the
-// start of two last lines were typed just like in Java
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 
+
+let result1 = '';
+for (let i = 0; i < 100; i++) {
+  result1 += (getRandomNumberFromRange(5,0)) + ', ';
+}
+console.log(result1.slice(0, result1.length - 2));
 
 
 // #2
 // Checks if length of string is not undefined
 // and is not bigger than non-negative max value
 function isStringLengthSuitable(string, maxLength) {
-  if (!(string == undefined || maxLength <= 0)) {
+  if (!(string === undefined || string === null || maxLength <= 0)) {
     return String(string).length <= maxLength;
   }
 }
 // https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/String
+
+
+let obj = {
+  str: "value",
+  num: 1
+};
+
+console.log(isStringLengthSuitable('hi!', 2));
+console.log(isStringLengthSuitable('hi!', 3));
+console.log(isStringLengthSuitable('hi!', 4));
+console.log(isStringLengthSuitable('hi!', 0));
+console.log(isStringLengthSuitable('hi!', -1));
+console.log(isStringLengthSuitable(null, 10));
+console.log(isStringLengthSuitable(undefined, 10));
+console.log(isStringLengthSuitable(obj, 10));
+console.log(isStringLengthSuitable(obj, 20));
