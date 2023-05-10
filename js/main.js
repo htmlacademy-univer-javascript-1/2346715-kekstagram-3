5907a6654cad5134ec2b597340c20e024f1d90a5
UlyssesVertpage <90066493+UlyssesVertpage@users.noreply.github.com>
2023-05-10 16:41:19 +0300

    Task 4, fix 2 for /js/main.js: Both functions were improved due to remarks of the eslint and their functionality modification.

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 
 // #1
-// Gets random number from range including boundaries by floor:
+// Gets random number from range including boundaries using floor rounding:
 function getRandomNumberFromRange(a, b) {
   if (a < 0 || b < 0) {
-    return -1;
+    return undefined;
   }
   return Math.floor((b >= a)
     ? Math.random() * (b + 1 - a) + a
@@ -11,36 +11,16 @@ function getRandomNumberFromRange(a, b) {
 }
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 
-
-let result1 = '';
-for (let i = 0; i < 100; i++) {
-  result1 += (getRandomNumberFromRange(5,0)) + ', ';
-}
-console.log(result1.slice(0, result1.length - 2));
+getRandomNumberFromRange(5, 0);
 
 
 // #2
-// Checks if length of string is not undefined
-// and is not bigger than non-negative max value
+// Checks if length of string is not bigger than non-negative max value
 function isStringLengthSuitable(string, maxLength) {
-  if (!(string === undefined || string === null || maxLength <= 0)) {
+  if (maxLength > 0) {
     return String(string).length <= maxLength;
   }
 }
 // https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/String
 
-
-let obj = {
-  str: "value",
-  num: 1
-};
-
-console.log(isStringLengthSuitable('hi!', 2));
-console.log(isStringLengthSuitable('hi!', 3));
-console.log(isStringLengthSuitable('hi!', 4));
-console.log(isStringLengthSuitable('hi!', 0));
-console.log(isStringLengthSuitable('hi!', -1));
-console.log(isStringLengthSuitable(null, 10));
-console.log(isStringLengthSuitable(undefined, 10));
-console.log(isStringLengthSuitable(obj, 10));
-console.log(isStringLengthSuitable(obj, 20));
+isStringLengthSuitable('hi!', 3);
