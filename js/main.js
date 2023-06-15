613a9ff7a5eabae93869a99b655ed797765a68a4
UlyssesVertpage <90066493+UlyssesVertpage@users.noreply.github.com>
2023-06-15 07:55:50 +0300

    createPhotosArray function was added

--- js/main.js
+++ js/main.js
@@ -1,27 +1,40 @@
-'use strict';
-
-
 
 // #1
 // Gets random number from range including boundaries by floor:
 function getRandomNumberFromRange(a, b) {
-  if (a < 0 || b < 0) return -1;
+  if (a < 0 || b < 0) {
+    return -1;
+  }
   return Math.floor((b >= a)
-      ? Math.random() * (b + 1 - a) + a
-      : Math.random() * (a + 1 - b) + b);
+    ? Math.random() * (b + 1 - a) + a
+    : Math.random() * (a + 1 - b) + b);
 }
 // A Code Convention wasn't read yet so two tabs at the
 // start of two last lines were typed just like in Java
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 
 
-
 // #2
 // Checks if length of string is not undefined
 // and is not bigger than non-negative max value
 function isStringLengthSuitable(string, maxLength) {
-  if (!(string == undefined || maxLength <= 0)) {
+  if (!(string === undefined || string === null || maxLength <= 0)) {
     return String(string).length <= maxLength;
   }
 }
 // https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/String
+
+
+function createPhotosArray() {
+  const arr = [];
+  for (let i = 1; i <= 25; i++) {
+    arr.push({
+      id: i,
+      url: `photos/${i}.jpg`,
+      description: 'Wonderful photo with a number '.concat(i),
+      likes: getRandomNumberFromRange(15, 200),
+      comments: getRandomNumberFromRange(0, 200)
+    });
+  }
+  return arr;
+}
